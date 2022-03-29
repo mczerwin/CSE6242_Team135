51184210150ef723f83a40d8e7a2228477d95804
Xuan Xu <shirleyxxb@foxmail.com>
2022-03-28 19:32:53 -0700

    added user parameter input shell (if not needed can delete later), enhanced getbuffer function, added markdown style, etc.

--- app.py
+++ app.py
@@ -2,38 +2,116 @@ import streamlit as st
 from PIL import Image
 import os
 import numpy as np
+import urllib.request
 
 
+@st.cache
 def load_image(image_file):
-    img = Image.open(image_file)
+    img = np.array(Image.open(image_file))
     return img
 
 
 def main():
-    st.title("File Upload Tutorial")
+    st.title('CSE6242 DVA Project 2022 Spring Team #135')
 
-    menu = ["Image","Dataset","DocumentFiles","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
+    st.markdown(
+        '''
+        <style>
+        [data-testid='stSidebar'][aria-expanded='true'] > div:first-child{
+            width: 350px
+        }
+        [data-testid='stSidebar'][aria-expanded='false'] > div:first-child{
+            width: 350px
+            margin-left: -350px
+        }
+        </style>
+        ''',
+        unsafe_allow_html=True,
+    )
+    st.sidebar.title('*customize later: Sidebar Title')
+    st.sidebar.subheader('*customize later: Sidebar subheader')
+    menu = ["Image", "Dataset", "DocumentFiles", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Image":
         st.subheader("Image")
-        image_file = st.file_uploader("Upload Images", tyipe=["jpeg"])
+        # if we want we can customize the sidebard format under choice=="Image", if needed
+        st.markdown(
+            """
+            <style>
+            [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+                width: 350px;
+            }
+            [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+                width: 350px;
+                margin-left: -350px;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+        image_file = st.file_uploader("Upload Images", type=["jpg", "jpeg", 'png'])
 
         if image_file is not None:
-            file_details = {"filename":image_file.name, "file_type":image_file.type, "filesize":image_file.size}
+
+            file_details = {"filename": image_file.name, "file_type": image_file.type, "filesize": image_file.size}
             st.write(file_details)
             img = load_image(image_file)
-            st.image(img,width=250)
+            st.image(img, width=250)
             st.write(np.shape(np.array(img)))
 
-            with open(os.path.join("images", image_file.name), "wb") as f:
-                f.write((image_file).getbuffer())
+            with open(os.path.join("./tempDir/", image_file.name), "wb") as f:
+                try:
+                    os.mkdir("./tempDir/", )
+                except OSError:
+                    f.write((image_file).getbuffer())
+        else:
+            # optional to show a demo picture here, if needed
+            urllib.request.urlretrieve(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/520px-Cat_poster_1.jpg',
+                "gfg.png")
+
+            img = Image.open("gfg.png")
+            st.image(img, width=250)
+            st.write(np.shape(np.array(img)))
+
+        st.sidebar.markdown('---')
+        num_inputbox = st.sidebar.number_input('Numeric parameter input box if needed', value=2, min_value=1)
+        st.sidebar.markdown('---')
+        float_slider = st.sidebar.slider('Float parameter slider if needed', min_value=0.0, max_value=1.0, value=0.5)
+
+    elif choice == "About":
+        st.markdown("In this application, we are user ***deep learning*** to...")
+        # if we want we can customize the sidebard format under choice=="About", if needed
+        st.markdown(
+            """
+            <style>
+            [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+                width: 350px;
+            }
+            [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+                width: 350px;
+                margin-left: -350px;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            '''
+            about this app section, use markdown rich text here\n
+
+            - [Youtube](https://www.youtube.com/watch?v=ObWSZ4rKUE0)
+            '''
+        )
+        st.video('https://www.youtube.com/watch?v=ouzwOd-TbRo')
+
 
     elif choice == "Dataset":
-        st.subheader("Dataset")
-    
+        st.markdown("Dataset")
+
     elif choice == "DocumentFiles":
-        st.subheader("DocumentFiles")
+        st.markdown("DocumentFiles")
 
 
 if __name__ == '__main__':
