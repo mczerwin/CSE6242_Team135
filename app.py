a4a6d7a896edb775294c79e5b5693a82643eda8f
Mike Czerwinski <mikeczerwinski@Mikes-MBP.home>
2022-04-13 12:41:56 -0400

    working inference on img upload

--- app.py
+++ app.py
@@ -7,15 +7,24 @@ import pickle
 from torchvision import transforms as T
 import torchvision.models as models
 import torch
+from dataSet import CustomDataset
 
 @st.cache
-def load_image(image_file):
-    img = np.array(Image.open(image_file))
-    return img
+def load_image(image_file, format):
+    if format == 'np':
+        return np.array(Image.open(image_file))
+    elif format == 'tensor':
+        return torch.utils.data.DataLoader(image_file)
+    else:
+        return image_file
 
+
+# Write image to disk
+def upload_image(img, upload_folder):
+    Image.open(img).save(f'./{upload_folder}/{img.name}')
 
 def load_model(filename):
-    return pickle.load(open(filename, 'rb'))
+    return torch.load(open(filename, 'rb'), map_location=torch.device('cpu'))
 
 
 def main():
@@ -60,21 +69,29 @@ def main():
         image_file = st.file_uploader("Upload Images", type=["jpg", "jpeg", 'png'])
 
         if image_file is not None:
+            
 
+            upload_image(image_file, 'uploads')
+
+            # Write some metadata about the image - sanity check that it is uploaded
             file_details = {"filename": image_file.name, "file_type": image_file.type, "filesize": image_file.size}
             st.write(file_details)
-            img = load_image(image_file)
-            st.image(img, width=250)
-            st.write(np.shape(np.array(img)))
+            img_np = load_image(image_file, 'np')
+            st.image(img_np, width=250)
+            st.write(np.shape(np.array(img_np)))
 
+            img = CustomDataset(csv_path='./row.csv', image_folder='./uploads', column_label='Pawpularity')
+            img = load_image(img, 'tensor')
             model = load_model('./eff_net.sav')
-            score = model.predict(img)
+            for input, label in img:
+                score = model(input)
+            print(score)
             st.write(f'Predicted Pawpularity: {score}')
-            with open(os.path.join("./tempDir/", image_file.name), "wb") as f:
-                try:
-                    os.mkdir("./tempDir/", )
-                except OSError:
-                    f.write((image_file).getbuffer())
+            # with open(os.path.join("./tempDir/", image_file.name), "wb") as f:
+            #     try:
+            #         os.mkdir("./tempDir/", )
+            #     except OSError:
+            #         f.write((image_file).getbuffer())
         else:
             # optional to show a demo picture here, if needed
             urllib.request.urlretrieve(
